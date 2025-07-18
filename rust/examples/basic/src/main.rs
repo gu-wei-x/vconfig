22a9366033e9b31ba7ffc7a5080bc3bc4fb01344
gu-wei-x <140672679+gu-wei-x@users.noreply.github.com>
2025-07-18 14:16:33 -0700

    Doc: add examples and docs (#7)
    Co-authored-by: Wei Gu <gu_wei_dev@outlook.com>

---
+++ rust/examples/basic/src/main.rs
@@ -0,0 +1,27 @@
+#![allow(dead_code)]
+use serde::Deserialize;
+use std::collections::HashMap;
+use std::error::Error;
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    key1: String,
+    key2: u64,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let raw_str = r#"
+        key1&variant1:v1 = "v1"
+        key1 = "v2"
+        key2&variant2:v2 = "5"
+        key2 = "3"
+    "#;
+    let mut variants = HashMap::new();
+    variants.insert("variant1".to_string(), "v1".to_string());
+    variants.insert("variant2".to_string(), "v2".to_string());
+    //let config_str: String = std::fs::read_to_string("basic.toml")?;
+    //println!("{config_str}");
+    let result = variants::from_str_with_variants::<Config, _>(raw_str, &variants);
+    println!("{:?}", result); // 
+    Ok(())
+}

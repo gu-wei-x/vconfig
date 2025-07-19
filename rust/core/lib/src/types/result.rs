16190b2d8b8bdf81fe8d17a4d85feb15991f8cc7
Wei Gu <gu_wei_dev@outlook.com>
2025-07-18 21:06:18 -0700

    [Primitive types]: support int&bool

--- rust/core/lib/src/types/result.rs
+++ rust/core/lib/src/types/result.rs
@@ -3,3 +3,9 @@
 use crate::types::error;
 
 pub type Result<T> = core::result::Result<T, error::Error>;
+
+impl<T> From<error::Error> for Result<T> {
+    fn from(error: error::Error) -> Self {
+        Err(error)
+    }
+}

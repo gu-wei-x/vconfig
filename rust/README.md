92e446d75d67642d1d64686bd2dc4cff51ef0ce6
Wei Gu <gu_wei_dev@outlook.com>
2025-07-20 20:44:55 -0700

    Add DefaultVariants

--- rust/README.md
+++ rust/README.md
@@ -4,8 +4,8 @@ variants is a crate to deserilize Rust data based on variant context.
 
 ```rust
 #![allow(dead_code)]
-use std::collections::HashMap;
 use std::error::Error;
+use variants::default::DefaultVariants;
 use variants::serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -28,18 +28,18 @@ fn from_str() -> Result<(), Box<dyn Error>> {
         key2&variant2:v2 = "5"
         key2 = "3"
     "#;
-    let mut variants = HashMap::new();
-    variants.insert("variant1".to_string(), "v1".to_string());
-    variants.insert("variant2".to_string(), "v2".to_string());
+    let mut variants = DefaultVariants::default();
+    _ = variants.add("variant1", "v1");
+    _ = variants.add("variant2", "v2");
     let result = variants::from_str_with_variants::<Config, _>(raw_str, &variants);
     println!("{:?}", result); // Ok(Config { key1: "v1", key2: 5 })
     Ok(())
 }
 
 fn from_file() -> Result<(), Box<dyn Error>> {
-    let mut variants = HashMap::new();
-    variants.insert("variant1".to_string(), "v1".to_string());
-    variants.insert("variant2".to_string(), "v2".to_string());
+    let mut variants = DefaultVariants::default();
+    _ = variants.add("variant1", "v1");
+    _ = variants.add("variant2", "v2");
     let result = variants::from_file_with_variants::<Config, _, _>("basic.toml", &variants);
     println!("{:?}", result); // Ok(Config { key1: "v1", key2: 5 })
     Ok(())
--- rust/core/lib/src/types/traits.rs
+++ rust/core/lib/src/types/traits.rs
@@ -2,7 +2,34 @@ pub trait Variants: Sized {
     fn matches(&self, variant: &Option<String>) -> bool;
 }
 
-impl Variants for std::collections::HashMap<String, String> {
+pub struct DefaultVariants {
+    variants: std::collections::HashMap<String, String>,
+}
+
+use crate::types::error::Error;
+use crate::types::result::Result;
+
+impl DefaultVariants {
+    pub fn add(&mut self, variant_name: &str, variant_value: &str) -> Result<()> {
+        let result = self
+            .variants
+            .insert(variant_name.to_owned(), variant_value.to_owned());
+        match result {
+            Some(_) => Ok(()),
+            None => Error::from_str("varaint exists").into(),
+        }
+    }
+}
+
+impl Default for DefaultVariants {
+    fn default() -> Self {
+        Self {
+            variants: std::collections::HashMap::new(),
+        }
+    }
+}
+
+impl Variants for DefaultVariants {
     fn matches(&self, variants: &Option<String>) -> bool {
         match variants {
             None => true,
@@ -10,7 +37,7 @@ impl Variants for std::collections::HashMap<String, String> {
                 let parts = str.split('&');
                 for part in parts {
                     if let Some((name, variant)) = part.split_once(':') {
-                        if let Some((_, value)) = self.get_key_value(name) {
+                        if let Some((_, value)) = self.variants.get_key_value(name) {
                             if variant != value {
                                 return false;
                             }

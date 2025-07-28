5988481e353bf820610e35293fe582861e31fd3b
Wei Gu <gu_wei_dev@outlook.com>
2025-07-28 15:38:23 -0700

    Change DefaultVariants to case insensitive

--- rust/core/lib/src/types/traits.rs
+++ rust/core/lib/src/types/traits.rs
@@ -11,9 +11,10 @@ use crate::types::result::Result;
 
 impl DefaultVariants {
     pub fn add(&mut self, variant_name: &str, variant_value: &str) -> Result<()> {
-        let result = self
-            .variants
-            .insert(variant_name.to_owned(), variant_value.to_owned());
+        let result = self.variants.insert(
+            variant_name.to_lowercase().to_owned(),
+            variant_value.to_owned(),
+        );
         match result {
             Some(_) => Ok(()),
             None => Error::from_str("varaint exists").into(),
@@ -37,7 +38,9 @@ impl Variants for DefaultVariants {
                 let parts = str.split('&');
                 for part in parts {
                     if let Some((name, variant)) = part.split_once(':') {
-                        if let Some((_, value)) = self.variants.get_key_value(name) {
+                        if let Some((_, value)) =
+                            self.variants.get_key_value(name.to_lowercase().as_str())
+                        {
                             if variant != value {
                                 return false;
                             }
